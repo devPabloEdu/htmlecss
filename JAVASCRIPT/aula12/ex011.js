37694723ab10791960d4d71fbfe15700eba660fd
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-04-16 15:55:27 -0300

    continuando

---
+++ JAVASCRIPT/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 40
+if(idade > 17){
+    console.log('voce vota')
+} else {
+    if (idade < 18 && idade > 15){
+        console.log('voce até pode votar')
+    } else{
+        console.log('voce definitivamente não  pode votar')
+    }
+} 
